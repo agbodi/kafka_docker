360e829b77370ec79ffe6f25a1d5e95272da443a
root <root@kafka.tobiloba.com>
2018-08-01 21:33:32 -0400

    Upload kafka v1 to hub

---
+++ kafka/kafka.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/opt/kafka_2.11-2.0.0/bin/kafka-server-start.sh config/server.properties 
+
---
+++ kafka/zoo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/opt/kafka_2.11-2.0.0/bin/zookeeper-server-start.sh config/zookeeper.properties 
+
+
